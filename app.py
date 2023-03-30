e94c5e81fcc3814ce2d451e51d57eff6a7949e3f
Natarajan <48755692+Natty07@users.noreply.github.com>
2023-03-30 18:42:53 +0200

    modified in course

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from github import Github, GithubIntegration
 
 app = Flask(__name__)
 
-app_id = '<your_app_number_here>'
+app_id = '311957'
 
 # Read the bot certificate
 with open(
-        os.path.normpath(os.path.expanduser('bot_key.pem')),
+        os.path.normpath(os.path.expanduser('/Users/natarajanchidambaram/Downloads/locbot-natarajan.2023-03-30.private-key.pem')),
         'r'
 ) as cert_file:
     app_key = cert_file.read()
